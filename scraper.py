4700719d21296a5b044e27c8c2a4791663f8ada5
leasennch <43566643+leasennch@users.noreply.github.com>
2018-10-26 13:48:25 +0200

    Hilfe von Plotti

--- scraper.py
+++ scraper.py
@@ -1,12 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
+import csv
 import datetime
 import scraperwiki
 
-df = pd.read_csv("skigebiete_linkliste.csv")
-linkliste = df['0'].tolist()
-
+linkliste = []
+reader = csv.reader(open("skigebiete_linkliste.csv"))
+for row in reader:
+    linkliste.append(row[0])
+    
 skigebiete_final = []
 for gebiet_link in linkliste:
     print(gebiet_link)
@@ -18,45 +20,9 @@ for gebiet_link in linkliste:
     titel = (titel.text.replace("Skigebiet ", ""))
     # Offene Skilifte auslesen
     for skigebiet in soup.find_all('table', class_="schneewerte"):
-        try:
-            offenelifte = (skigebiet.find_all('td')[-1].text).split(" von ") # Auslesen im Format "12 von 24" (offene Lifte sind immer das letzte TD, deshalb -1)
-            total = offenelifte[1].strip()
-            offen = offenelifte[0].strip()
-            now = datetime.datetime.now()
-            scraperwiki.sqlite.save(unique_keys=['name'], data={"zeit": now.strftime("%Y-%m-%d_%H-%M"), "titel": titel, "totallifte": total, "offen": offen})
-            #skigebiete_final.append({"titel": titel, "totallifte": total, "offen": offen}) # In die finale Liste laden
-        except:
-            print("Fehler mit Skigebiet: "+"https://www.bergfex.ch"+gebiet_link)
+        offenelifte = (skigebiet.find_all('td')[-1].text).split(" von ") # Auslesen im Format "12 von 24" (offene Lifte sind immer das letzte TD, deshalb -1)
+        total = offenelifte[1].strip()
+        offen = offenelifte[0].strip()
+        now = datetime.datetime.now()
+        scraperwiki.sqlite.save(unique_keys=['name'], data={"zeit": now.strftime("%Y-%m-%d_%H-%M"), "titel": titel, "totallifte": total, "offen": offen})
 
-
-# Write out to the sqlite database using scraperwiki library
-#df_skigebiete.to_csv('skigebiete_oeffnungszeiten/'+now.strftime("%Y-%m-%d_%H-%M_skigebiete_oeffnungszeiten.csv"))
-
-# import scraperwiki
-#import lxml.html
-
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[10]:
-
-
-# In[ ]:
-
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
